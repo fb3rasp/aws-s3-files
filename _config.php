0a6678a23f9125a93b782f4c94136e68ddc5a66c
Rainer Spittel <rainer.spittel@gmail.com>
2014-12-17 12:08:36 +0100

    ENHANCEMENT add ability to define the region via config.

--- _config.php
+++ _config.php
@@ -3,9 +3,9 @@
 
 //
 // configure s3 storage for file update/download
-//
 if (defined('S3FILE_ACCESS_KEY_ID'))
 {
     AWS_S3File::set_auth(S3FILE_ACCESS_KEY_ID, S3FILE_SECRET_ACCESS_KEY);
     AWS_S3File::set_default_bucket(S3FILE_DEFAULTBUCKET);
+    AWS_S3File::set_region(S3FILE_REGION);
 }
