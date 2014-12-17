4da9e673a350686b31514ceaf01de12136834ba7
Rainer Spittel <rainer.spittel@gmail.com>
2014-12-17 17:24:47 +0100

    ENHANCEMENT add ability to use regular file-validations on the new s3file objects.

--- code/AWS_S3UploadField.php
+++ code/AWS_S3UploadField.php
@@ -130,6 +130,30 @@ class AWS_S3UploadField extends UploadField
             'UploadFieldFileButtons' => (string)$file->renderWith($this->getTemplateFileButtons())
         ));
     }
+
+
+    /**
+     * Gets the foreign class that needs to be created, or 'File' as default if there
+     * is no relationship, or it cannot be determined.
+     *
+     * @param $default Default value to return if no value could be calculated
+     * @return string Foreign class name.
+     */
+    public function getRelationAutosetClass($default = 'AWS_S3File') {
+
+        // Don't autodetermine relation if no relationship between parent record
+        if(!$this->relationAutoSetting) return $default;
+
+        // Check record and name
+        $name = $this->getName();
+        $record = $this->getRecord();
+        if(empty($name) || empty($record)) {
+            return $default;
+        } else {
+            $class = $record->getRelationClass($name);
+            return empty($class) ? $default : $class;
+        }
+    }
 }
 
 /**
