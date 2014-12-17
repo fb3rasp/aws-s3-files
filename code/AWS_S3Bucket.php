64143b1f897d00af16d7fae0aa33318c1637c1ff
Rainer Spittel <rainer.spittel@gmail.com>
2014-12-17 12:09:10 +0100

    MINOR added phpdoc

--- code/AWS_S3Bucket.php
+++ code/AWS_S3Bucket.php
@@ -12,7 +12,13 @@ class AWS_S3Bucket extends DataObject {
         "Name" => "Varchar"
     );
 
-
+    /**
+     * This method checks if a given bucket name does exist as dataobject (AWS_S3Bucket) and if it doesn't create one.
+     *
+     * @param $bucketName
+     *
+     * @return DataObject
+     */
     public static function find_or_make($bucketName)
     {
         $list = DataList::create('AWS_S3Bucket')->where("\"Name\" = '{$bucketName}'");
@@ -23,7 +29,6 @@ class AWS_S3Bucket extends DataObject {
             $bucket->Name = $bucketName;
             $bucket->write();
         }
-
         return $bucket;
     }
 } 
