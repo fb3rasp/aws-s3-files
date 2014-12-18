6e0c9cd0da74f27627f99e2fd78427b6341876fa
Rainer Spittel <rainer.spittel@gmail.com>
2014-12-18 16:09:22 +0100

    MINOR add test download controller. Only accessible in DEV environment. call /[BaseURL]/DownloadController to see all available files (stored S3Files). Click on a link to download the file.

---
+++ code/DownloadController.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: rainerspittel
+ * Date: 18/12/14
+ * Time: 3:22 PM
+ */
+
+/**
+ * Class DownloadController
+ *
+ * Test controller classe
+ */
+class DownloadController extends Controller
+{
+
+    static private $allowed_actions = array(
+        'download',
+        'index'
+    );
+
+    public function init()
+    {
+        parent::init();
+        if (!Director::isDev()) {
+            user_error('Test controller only available in test dev-environment',E_USER_ERROR);
+        }
+    }
+
+    public function index()
+    {
+        $list = DataList::create('AWS_S3File');
+
+        echo "<h1>Available Files</h1>";
+        echo "<ul>";
+        foreach($list as $item)
+        {
+            echo sprintf("<li><a href='download/%s'>Download %s</a></li>", $item->ID,$item->Name);
+
+        }
+        echo "</ul>";
+    }
+
+    public function download(SS_HTTPRequest $request)
+    {
+        $Id = $request->param('ID');
+        $file = DataList::create('AWS_S3File')->filter(
+            array(
+                'ID' => (int)$Id
+            )
+        )->First();
+
+        if ($file)
+        {
+            $response = $file->downloadFile($this);
+            return $response;
+        }
+    }
+
+} 
